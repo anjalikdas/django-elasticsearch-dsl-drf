c75f021754b158b7a2376aa395b1f23b510c33f4
Anjali K <anjali.k@fayausa.com>
2025-03-17 11:23:05 +0530

    fix(src): fix aggsproxy import error

--- src/django_elasticsearch_dsl_drf/utils.py
+++ src/django_elasticsearch_dsl_drf/utils.py
@@ -3,16 +3,20 @@ Utils.
 """
 
 import datetime
-from elasticsearch_dsl.search import AggsProxy
+
+try:
+    from elasticsearch_dsl.search import AggsProxy
+except ImportError:
+    from elasticsearch_dsl.search_base import AggsProxy
 
 
-__title__ = 'django_elasticsearch_dsl_drf.utils'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = '2017-2020 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
+__title__ = "django_elasticsearch_dsl_drf.utils"
+__author__ = "Artur Barseghyan <artur.barseghyan@gmail.com>"
+__copyright__ = "2017-2020 Artur Barseghyan"
+__license__ = "GPL 2.0/LGPL 2.1"
 __all__ = (
-    'DictionaryProxy',
-    'EmptySearch',
+    "DictionaryProxy",
+    "EmptySearch",
 )
 
 
@@ -20,7 +24,7 @@ class EmptySearch(object):
     """Empty Search."""
 
     def __init__(self, *args, **kwargs):
-        self.aggs = AggsProxy('')
+        self.aggs = AggsProxy("")
         self._highlight = {}
         self._sort = []
         self.total = 0
